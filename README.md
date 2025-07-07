140e667d5ca12d9911e550cd4d8ed5923b6c1d30
fardinbaf <fardinbaf@gmail.com>
2025-07-07 20:16:30 +0600

    Initial commit from

--- README.md
+++
@@ -1,3 +0,0 @@
-# sb1-rckxdhef
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/fardinbaf/sb1-rckxdhef)
--- src/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedRoute = () => {
-  const isAuthenticated = sessionStorage.getItem('isAuthenticated') === 'true';
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/admin/login" replace />;
-};
-
-export default ProtectedRoute;
