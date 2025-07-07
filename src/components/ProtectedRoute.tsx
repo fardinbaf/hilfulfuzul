02c2fa9af31a915543c0be625a7cfebff2d86ed8
fardinbaf <90677152+fardinbaf@users.noreply.github.com>
2025-07-07 23:47:07 +0600

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,8 +2,16 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useData } from '../context/DataContext';
 
 const ProtectedRoute = () => {
-    const { isAuthenticated } = useData();
+    const { isAuthenticated, isLoading } = useData();
     const location = useLocation();
+
+    if (isLoading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-emerald-600"></div>
+            </div>
+        );
+    }
 
     if (!isAuthenticated) {
         return <Navigate to="/admin/login" state={{ from: location }} replace />;
