6ddf0039650185db35a76421ecdcf14fffdc53dd
Fardin <90677152+fardinbaf@users.noreply.github.com>
2026-03-18 20:59:56 +0600

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <div className="h-10 w-10">
       <img 
-        src="https://images4.imagebam.com/79/fd/33/ME11HS7P_o.png" 
+        src="https://images4.imagebam.com/47/20/f6/ME1BFBCK_o.png" 
         alt="হিলফুল ফুজুল সংঘ Logo" 
         className="h-full w-full object-contain"
       />
